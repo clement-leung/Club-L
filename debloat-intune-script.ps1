8ecd392a0f17b28d28ae012ccf2c9fcacfbac414
clement-leung <clement.leung@clubllondon.com>
2024-09-17 17:16:01 +0100

    Update debloat-intune-script.ps1

--- debloat-intune-script.ps1
+++ debloat-intune-script.ps1
@@ -1,25 +1,28 @@
+# Define the path for the debloat folder
 $DebloatFolder = "C:\ProgramData\Debloat"
+
+# Check if the folder exists
 If (Test-Path $DebloatFolder) {
     Write-Output "$DebloatFolder exists. Skipping."
-}
-Else {
+} Else {
     Write-Output "The folder '$DebloatFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
-    Start-Sleep 1
-    New-Item -Path "$DebloatFolder" -ItemType Directory
+    Start-Sleep -Seconds 1  # Specify the unit of time for clarity
+    New-Item -Path $DebloatFolder -ItemType Directory -Force  # Use -Force to handle existing paths
     Write-Output "The folder $DebloatFolder was successfully created."
 }
 
+# Define the path for the template file
 $templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
 
+# Download the script
 Invoke-WebRequest `
--Uri "https://raw.githubusercontent.com/clement-leung/Club-L/main/RemoveBloat.ps1" `
--OutFile $templateFilePath `
--UseBasicParsing `
--Headers @{"Cache-Control"="no-cache"}
+    -Uri "https://raw.githubusercontent.com/clement-leung/Club-L/main/RemoveBloat.ps1" `
+    -OutFile $templateFilePath `
+    -UseBasicParsing `
+    -Headers @{"Cache-Control"="no-cache"}
 
-
-##Populate between the speechmarks any apps you want to whitelist, comma-separated
+# Populate between the speech marks any apps you want to whitelist, comma-separated
 $arguments = ' -customwhitelist ""'
 
-
-invoke-expression -Command "$templateFilePath $arguments"
+# Execute the downloaded script with the specified arguments
+& $templateFilePath $argumentsBloat
